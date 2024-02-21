09769bf78d51baffbaf56331fa55013ab08dde65
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-02-21 22:55:55 +0100

    update set_dost.sh

--- set_dots.sh
+++ set_dots.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Create links do dotfiles
 cd "$HOME/git/arch-install-hyprland/dotfiles" || exit
 stow --adopt -t "$HOME" .
+
+# Create links to etc files
+cd "$HOME/git/arch-install-hyprland/etc" || exit
+sudo stow --adopt -t /etc .
+
+# Reset to master branch 
 git reset --hard
 git pull
-cd "$HOME"
+cd "$HOME/git/arch-install-hyprland"
