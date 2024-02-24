87ce82c4b9e69743558f21fc00696263c12d6b13
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-02-24 21:54:30 +0100

    added gtk4 theme

--- install.sh
+++ install.sh
@@ -175,23 +175,6 @@ if command -v git &> /dev/null; then
   git config --global init.defaultBranch main
 fi
 
-# Create theme based on Adwaita
-read -p "Build theme based on Adwaita? [Y/n] " yn
-if [[ $yn == "Y" || $yn == "y" || $yn == "" ]]; then
-  # Clone Adwaita repository directory
-  mkdir -p $SCRIPT_DIR/../adwaita-custom
-  cd $SCRIPT_DIR/../adwaita-custom || exit
-  git init
-  git remote add -f origin https://gitlab.gnome.org/GNOME/gtk.git
-  git config core.sparseCheckout true
-  echo "gtk/theme/Adwaita" >> .git/info/sparse-checkout
-  gtk3_version=$(pacman -Qi gtk3 | sed -nr 's/Version.*:(.*)-.*$/\1/p')
-  git fetch origin refs/tags/$gtk3_version
-  git checkout $gtk3_version
-
-  
-fi
-
 # Enable Docker if installed
 read -p "Enable Docker? [Y/n] " yn
 if [[ $yn == "Y" || $yn == "y" || $yn == "" ]]; then
