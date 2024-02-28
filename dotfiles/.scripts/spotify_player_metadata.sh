57bd668af89965d7857183950aa545ac10089403
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-02-28 23:47:33 +0100

    custom playerctl script add icon

--- dotfiles/.scripts/spotify_player_metadata.sh
+++ dotfiles/.scripts/spotify_player_metadata.sh
@@ -2,5 +2,12 @@
 
 player=$(playerctl --list-all | grep spotify | head -n 1)
 result=$(playerctl --player=$player metadata --format "{{ title }} - {{ artist }}")
+status=$(playerctl --player=$player status)
+
+if [ "$status" = "Playing" ]; then
+  result=" $result"
+else
+  result=" $result"
+fi
 
 echo "{\"text\": \"${result}\", \"class\": \"spotify-text\"}"
