6f83e2b4b20eb7dbec30e324441a7f73290a5dba
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-02-28 23:44:35 +0100

    custom playerctl script fix

--- dotfiles/.scripts/spotify_player_metadata.sh
+++ dotfiles/.scripts/spotify_player_metadata.sh
@@ -3,4 +3,4 @@
 player=$(playerctl --list-all | grep spotify | head -n 1)
 result=$(playerctl --player=$player metadata --format "{{ title }} - {{ artist }}")
 
-echo "{text: \"${result}\", class: \"spotify-text\"}"
+echo "{\"text\": \"${result}\", \"class\": \"spotify-text\"}"
