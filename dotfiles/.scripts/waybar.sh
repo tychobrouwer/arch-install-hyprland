cd21325530743afc93280d3e02a2488e98cdac60
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-07-13 00:01:29 +0200

    waybar to jsonc

--- dotfiles/.scripts/waybar.sh
+++ dotfiles/.scripts/waybar.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/waybar/waybar.json $HOME/.config/waybar/waybar.css"
+CONFIG_FILES="$HOME/.config/waybar/waybar.jsonc $HOME/.config/waybar/waybar.css"
 
 trap "killall waybar" EXIT
 
 if [[ ! $(pidof waybar) ]]; then
   while true; do
-    waybar -c $HOME/.config/waybar/waybar.json -s $HOME/.config/waybar/waybar.css &
+    waybar -c $HOME/.config/waybar/waybar.jsonc -s $HOME/.config/waybar/waybar.css &
 
     if [[ ! $(pidof waybar) ]]; then
       notify-send -u critical -a "Waybar" "Failed to start Waybar"
