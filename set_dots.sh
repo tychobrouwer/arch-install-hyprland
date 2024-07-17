217d7b043a036d4811124cfe080723423024f965
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-07-17 19:07:43 +0200

    nvidia drivers

--- set_dots.sh
+++ set_dots.sh
@@ -2,15 +2,16 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Create links do dotfiles
+# Create directories needed for dotfiles
 tree -dfi --noreport $SCRIPT_DIR/dotfiles | xargs -I {} mkdir -p "{}"
 cd "$SCRIPT_DIR/dotfiles" || exit
+# Create links to dotfiles
 stow --adopt -t "$HOME" .
 
 # Copy files to /etc
 sudo cp -r $SCRIPT_DIR/etc/* /etc
 
-# Reset to master branch 
+# Reset to master branch
 git reset --hard
 git pull
 cd "$SCRIPT_DIR"
