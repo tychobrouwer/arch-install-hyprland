a922c0ccd18c9c32a862df4ff3748baad295c5c8
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-09-15 23:51:51 +0200

    fix spotify launching maybe + add unreal alias

--- dotfiles/.scripts/spotify_player.sh
+++ dotfiles/.scripts/spotify_player.sh
@@ -2,6 +2,17 @@
 
 first=true
 
+while true; do 
+  ping -c1 www.google.com &> /dev/null && break;
+  sleep 1;
+done
+
+
+#while ! ip route | grep -oP 'default via .+ dev'; do
+#  echo "interface not up, will try again in 1 second";
+#  sleep 1;
+#done
+
 while true; do
   if [[ ! $(pidof spotify_player) ]]; then
     spotify_player -d --config-folder $HOME/.config/spotify_player
