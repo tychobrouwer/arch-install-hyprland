55e1344c33cfb220bba8b7af0855c20bea1726e7
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-09-23 15:27:00 +0200

    remove which-keys

--- dotfiles/.config/nvim/lua/config/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/config/plugins/telescope.lua
@@ -5,11 +5,18 @@ return {
   },
   config = function()
     local builtin = require('telescope.builtin')
-    
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    local utils = require("telescope.utils")
+
+    vim.keymap.set('n', '<leader>pf', function()
+      builtin.find_files({ hidden = true, cwd = utils.buffer_dir() })
+    end)
+
+    vim.keymap.set('n', '<C-p>', function ()
+      builtin.git_files({ hidden = true })
+    end)
+
     vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      builtin.grep_string({ search = vim.fn.input("Grep > "), hidden = true })
     end)
   end
 }
