d1d188fdaecbb17fd6351f474fdefa340ee4f3d5
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-09-23 16:22:45 +0200

    remove nvim keysbinds rofi

--- dotfiles/.config/nvim/lua/config/plugins/harpoon.lua
+++ dotfiles/.config/nvim/lua/config/plugins/harpoon.lua
@@ -10,9 +10,14 @@ return {
     vim.keymap.set("n", "<leader>a", mark.add_file)
     vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
 
+    vim.keymap.set("n", "<leader>!", function() mark.set_current_at(1) end)
+    vim.keymap.set("n", "<leader>@", function() mark.set_current_at(2) end)
+    vim.keymap.set("n", "<leader>#", function() mark.set_current_at(3) end)
+    vim.keymap.set("n", "<leader>$", function() mark.set_current_at(4) end)
+
     vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
     vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
     vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)   
+    vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
   end
 }
