cbd3c7878770053c717d85fa38cfa382ef98ed2c
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-10-23 10:42:29 +0200

    update spotify + fis settings + fix external monitor settings

--- dotfiles/.scripts/spotify_player_metadata.sh
+++ dotfiles/.scripts/spotify_player_metadata.sh
@@ -3,4 +3,4 @@ player=$(playerctl --list-all | grep spotify | head -n 1)
 
 while read -r line; do 
   echo "{\"text\": \"$line\", \"class\": \"spotify-text\"}" | sed -e 's/Playing//' -e 's/Paused//'
-done < <(playerctl --player=$player --follow metadata --format "{{ status }} {{ markup_escape(title) }} - {{ artist }}" )
+done < <(playerctl --player=spotify_player --follow metadata --format "{{ status }} {{ markup_escape(title) }} - {{ artist }}" )
