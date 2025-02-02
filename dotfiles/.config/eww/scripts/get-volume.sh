2e55366b1a323109408ff38cf286f241996e421c
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-02-02 20:58:03 +0100

    fix volume widget

--- dotfiles/.config/eww/scripts/get-volume.sh
+++ dotfiles/.config/eww/scripts/get-volume.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-if
-    command -v wpctl &
-    >/dev/null
-then
+if command -v wpctl &>/dev/null; then
     if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q "MUTED"; then
         echo 0
         exit
     else
         wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}'
     fi
-elif
-    command -v pamixer &
-    >/dev/null
-then
+elif command -v pamixer &>/dev/null; then
     if [ true == $(pamixer --get-mute) ]; then
         echo 0
         exit
