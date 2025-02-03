e5f2c810f86de5828581dda7d35cdec9931adbcb
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-02-03 18:57:48 +0100

    fix errors test

--- dotfiles/.config/eww/scripts/music-info.sh
+++ dotfiles/.config/eww/scripts/music-info.sh
@@ -89,7 +89,7 @@ get_time() {
 	local current_time=$(echo $PLAYBACK_DATA | jq -r .progress_ms)
 	local total_time=$(echo $PLAYBACK_DATA | jq -r .item.duration_ms)
 
-	if [ -z "$current_time" || -z "$total_time" || "$total_time" -eq 0 ]; then
+	if [ -z "$current_time" ] || [ -z "$total_time" ] || [ "$total_time" -eq 0 ]; then
 		echo 0
 	else
 		local progress=$((100 * current_time / total_time))
