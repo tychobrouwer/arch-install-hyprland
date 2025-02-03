ffa53fa2758300571d5f8fecc697337b821c686e
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-02-03 15:49:57 +0100

    fix errrors eww bar music

--- dotfiles/.config/eww/scripts/music-info.sh
+++ dotfiles/.config/eww/scripts/music-info.sh
@@ -36,12 +36,13 @@ get_time() {
 	local current_time=$(echo $PLAYBACK_DATA | jq -r .progress_ms)
 	local total_time=$(echo $PLAYBACK_DATA | jq -r .item.duration_ms)
 	
-	if [ -z "$current_time" ] || [ -z "$total_time" ] || [ "$total_time" -eq 0 ]; then
+	if [ -z "$current_time" ] || [ -z "$total_time" ] || [[ "$total_time" == "null" ]] || [[ "$current_time" == "null" ]] || [ "$total_time" -eq 0 ]; then
 		echo "0"
-	else
-		local progress=$((100 * current_time / total_time))
-		echo "$progress"
+		return
 	fi
+	
+	local progress=$((100 * current_time / total_time))
+	echo "$progress"
 }
 
 get_ctime() {
